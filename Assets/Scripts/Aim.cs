0027291adf2dff1b9a9167e18153c3c30d0bf38c
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-03-24 11:24:37 -0300

    Now the pistons look at the leg point

---
+++ Assets/Scripts/Aim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    public Transform TransformToAim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(TransformToAim);
+    }
+}
