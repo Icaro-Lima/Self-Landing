f96b5e4dbf7eab6660dd4f436347c548062d91cc
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-04-17 00:46:27 -0300

    Adds the blue portal

---
+++ Assets/Portals/PortalRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalRotation : MonoBehaviour
+{
+    readonly float Speed = -90;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * Speed);
+    }
+}
