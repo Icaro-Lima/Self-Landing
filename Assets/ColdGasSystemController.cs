ff44c2658d913f21af716bfcc13dbcd5f71681c3
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-04-24 23:39:45 -0300

    Fixes the ColdGas script and the center of mass

---
+++ Assets/ColdGasSystemController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColdGasSystemController : MonoBehaviour
+{
+    Rigidbody RocketRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RocketRigidbody = GetComponentInParent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/FixCenterOfMass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixCenterOfMass : MonoBehaviour
+{
+    public Transform CenterOfMassTransform;
+
+    Rigidbody Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        Rigidbody.centerOfMass = CenterOfMassTransform.localPosition;
+    }
+}
