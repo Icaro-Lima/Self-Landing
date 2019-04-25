39574621926545d8481897c493d88ade03890fc4
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-04-25 01:20:23 -0300

    ColdGas Roll and Pitch (with fisic problems)

--- Assets/ColdGasSystemController.cs
+++ Assets/ColdGasSystemController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ColdGasSystemController : MonoBehaviour
 {
+    public ColdGasThrusterService[] ColdGasThrusterServices;
+
     Rigidbody RocketRigidbody;
 
     // Start is called before the first frame update
@@ -15,6 +17,28 @@ public class ColdGasSystemController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Keypad7))
+        {
+            ColdGasThrusterServices[0].Left();
+            ColdGasThrusterServices[2].Left();
+        }
 
+        if (Input.GetKey(KeyCode.Keypad9))
+        {
+            ColdGasThrusterServices[0].Right();
+            ColdGasThrusterServices[2].Right();
+        }
+
+        if (Input.GetKey(KeyCode.Keypad8))
+        {
+            ColdGasThrusterServices[0].Down();
+            ColdGasThrusterServices[2].Up();
+        }
+
+        if (Input.GetKey(KeyCode.Keypad2))
+        {
+            ColdGasThrusterServices[0].Up();
+            ColdGasThrusterServices[2].Down();
+        }
     }
 }
---
+++ Assets/ColdGasThrusterService.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColdGasThrusterService : MonoBehaviour
+{
+    public ParticleSystem ThrusterUp;
+    public ParticleSystem ThrusterDown;
+    public ParticleSystem ThrusterBackward;
+    public ParticleSystem ThrusterRight;
+    public ParticleSystem ThrusterLeft;
+
+    Rigidbody RocketRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RocketRigidBody = GetComponentInParent<Rigidbody>();
+    }
+
+    public void Right()
+    {
+        ThrusterRight.Play();
+        RocketRigidBody.AddForceAtPosition(-ThrusterRight.transform.forward, ThrusterRight.transform.position);
+    }
+
+    public void Left()
+    {
+        ThrusterLeft.Play();
+        RocketRigidBody.AddForceAtPosition(-ThrusterLeft.transform.forward, ThrusterLeft.transform.position);
+    }
+
+    public void Up()
+    {
+        ThrusterUp.Play();
+        RocketRigidBody.AddForceAtPosition(-ThrusterUp.transform.forward, ThrusterUp.transform.position);
+    }
+
+    public void Down()
+    {
+        ThrusterDown.Play();
+        RocketRigidBody.AddForceAtPosition(-ThrusterDown.transform.forward, ThrusterDown.transform.position);
+    }
+}
