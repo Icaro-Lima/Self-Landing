fc786cf5fda3b75a8d6b7038674847b7e0e67bf7
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-04-27 17:59:38 -0300

    Adds the rocket thruster animation

---
+++ Assets/JetEngineAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetEngineAnimation : MonoBehaviour
+{
+    public float Force = 1;
+
+    Light Light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Light = GetComponentInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(Force, 4 * Force, Force);
+        Light.range = 5 * Force;
+    }
+}
