6723a57d50a9897851f9f8a0eb3b34a9c9217516
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-04-26 23:07:48 -0300

    Adds comments

--- Assets/ACSNozzleAssemblyService.cs
+++ Assets/ACSNozzleAssemblyService.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Essa classe representa as funções básicas de um ACS Nozzle Assembly.
+/// Para se ter uma boa ideia, uma imagem do componente pode ser encontrada aqui:
+/// https://drive.google.com/file/d/1Zw4vSG_TJJjkhdWT8bdmUphTVT_NcIHI/view?usp=sharing
+/// 
+/// Um ACS Nozzle Assembly pode ser usado para manobrar o foguete, jogando gás comprimido contra o
+/// movimento desejado.
+/// </summary>
 public class ACSNozzleAssemblyService : MonoBehaviour
 {
     public ParticleSystem ThrusterUp;
@@ -10,32 +18,51 @@ public class ACSNozzleAssemblyService : MonoBehaviour
     public ParticleSystem ThrusterRight;
     public ParticleSystem ThrusterLeft;
 
+    /// <summary>
+    /// O RigidBody (controlador de física) do foguete. Note que essa classe deve estar atrelada a
+    /// um GameObject dentro do foguete.
+    /// </summary>
     Rigidbody RocketRigidBody;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// O Start é um evento do Unity que é chamado assim que o objeto é criado. Nesse caso ele está
+    /// sendo usado para pegar o RigidBody do foguete.
+    /// </summary>
     void Start()
     {
         RocketRigidBody = GetComponentInParent<Rigidbody>();
     }
 
+    /// <summary>
+    /// Libera gás comprimido do bocal da direita, gerando movimento para esquerda.
+    /// </summary>
     public void Right()
     {
         ThrusterRight.Play();
         RocketRigidBody.AddForceAtPosition(-ThrusterRight.transform.forward, ThrusterRight.transform.position);
     }
 
+    /// <summary>
+    /// Libera gás comprimido do bocal da esquerda, gerando movimento para direita.
+    /// </summary>
     public void Left()
     {
         ThrusterLeft.Play();
         RocketRigidBody.AddForceAtPosition(-ThrusterLeft.transform.forward, ThrusterLeft.transform.position);
     }
 
+    /// <summary>
+    /// Libera gás comprimido do bocal de cima, gerando movimento para baixo.
+    /// </summary>
     public void Up()
     {
         ThrusterUp.Play();
         RocketRigidBody.AddForceAtPosition(-ThrusterUp.transform.forward, ThrusterUp.transform.position);
     }
 
+    /// <summary>
+    /// Libera gás comprimido do bocal de baixo, gerando movimento para cima.
+    /// </summary>
     public void Down()
     {
         ThrusterDown.Play();
--- Assets/ACSService.cs
+++ Assets/ACSService.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// O intuito dessa classe é fornecer funções para manipular todo o conjunto ACS. Uma imagem pode
+/// ser obtida aqui: https://www.nasa.gov/images/content/409261main_roll_control_animation_226.jpg.
+/// </summary>
 public class ACSService : MonoBehaviour
 {
-    public ACSNozzleAssemblyService[] ColdGasThrusterServices;
+    /// <summary>
+    /// Uma lista de todas as montagens de bocal (ACS Nozzle Assembly), no nosso caso são 4.
+    /// </summary>
+    public ACSNozzleAssemblyService[] ColdGasThrusterServices; // TODO: Renomear.
 
+    /// <summary>
+    /// O RigidBody (controlador de física) do foguete. Note que essa classe deve estar atrelada a
+    /// um GameObject dentro do foguete.
+    /// </summary>
     Rigidbody RocketRigidbody;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// O Start é um evento do Unity que é chamado assim que o objeto é criado. Nesse caso ele está
+    /// sendo usado para pegar o RigidBody do foguete.
+    /// </summary>
     void Start()
     {
         RocketRigidbody = GetComponentInParent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    // TODO: Esse método deve ser removido, está sendo usado temporariamente apenas para testes.
     void Update()
     {
         if (Input.GetKey(KeyCode.Keypad7))
