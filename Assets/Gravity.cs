00a9e10dac2813f4d5cf52e9ce3281fef6b875bc
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-04-27 22:42:00 -0300

    Adds gravity to the rocket

---
+++ Assets/Gravity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    Rigidbody Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody.AddForce((Vector3.zero - Rigidbody.transform.position).normalized * 9.81f, ForceMode.Acceleration);
+    }
+}
