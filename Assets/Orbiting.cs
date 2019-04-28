a476c435a72d3022cc04567f00d1db9642656b57
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-04-28 02:28:17 -0300

    Rocket orbiting!

---
+++ Assets/Orbiting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiting : MonoBehaviour
+{
+    Rigidbody Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+
+        Rigidbody.AddForce(transform.up * 10000, ForceMode.Acceleration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
