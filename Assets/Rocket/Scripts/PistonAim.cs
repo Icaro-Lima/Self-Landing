bdefdf18a0f8ee7866a3ee4c91b58e7fba635ff3
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-04-28 23:59:27 -0300

    Now pistons works in edit mode

--- Assets/Rocket/Scripts/PistonAim.cs
+++ Assets/Rocket/Scripts/PistonAim.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class PistonAim : MonoBehaviour
 {
     public Transform TransformToAim;
--- Assets/Rocket/Scripts/PistonsResize.cs
+++ Assets/Rocket/Scripts/PistonsResize.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class PistonsResize : MonoBehaviour
 {
     public Transform Target;
