ed1fa55760371f2fd82ca35dbc89b47e94041b2a
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-06-22 20:05:01 -0300

    Angle freezed in the XZ plane. Position freezed in the Y axis.

---
+++ Assets/FreezeToXZ.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeToXZ : MonoBehaviour
+{
+    Vector3 startingEulerAngles;
+    Vector3 startingPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingEulerAngles = transform.eulerAngles;
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        transform.eulerAngles = new Vector3(startingEulerAngles.x, transform.eulerAngles.y, startingEulerAngles.z);
+        transform.position = new Vector3(transform.position.x, startingPosition.y, transform.position.z);
+    }
+}
